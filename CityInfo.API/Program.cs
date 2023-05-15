394d100cf5c0c33001cc29f30e8504228a292886
umermasood <umermasood.dev@gmail.com>
2023-05-15 16:39:38 +0500

    03 - Data Formatters and Content Negotiation (Add support for XML to API)

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
-});
+}).AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
